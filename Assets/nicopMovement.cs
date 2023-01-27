82b08b4c494b039bb5221d3a5871d15a5b56fc75
NicoBeade <75858058+NicoBeade@users.noreply.github.com>
2023-01-26 23:43:09 -0300

    Added flapping

--- Assets/nicopMovement.cs
+++ Assets/nicopMovement.cs
@@ -2,17 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class nicopMovement : MonoBehaviour
-{
+public class nicopMovement : MonoBehaviour{
+
+    public Rigidbody2D myRigidbody;
+    public float flapStrength;
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start(){
         
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update(){
+
+
+        if (Input.GetKeyDown(KeyCode.Space)){
+            myRigidbody.velocity = Vector2.up * flapStrength;
+        }
+
+
     }
 }
