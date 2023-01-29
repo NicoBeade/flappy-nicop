4480f9814f08c45dee3046707475987eeb85a07d
NicoBeade <75858058+NicoBeade@users.noreply.github.com>
2023-01-29 01:44:04 -0300

    Final touches, se exporto el juego :)

--- Assets/Scripts/LogicScript.cs
+++ Assets/Scripts/LogicScript.cs
@@ -2,16 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
 public class LogicScript : MonoBehaviour
 {
     // Start is called before the first frame update
     public int playerScore;
     public Text scoreText;
+    public GameObject gameOverScreen;
+    public int scoreToAdd = 1;
     [ContextMenu("Increase Score")]
      
     public void addScore()
     {
-        playerScore++;
+        playerScore+=scoreToAdd;
         scoreText.text = playerScore.ToString();
     }
+
+    public void restartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void gameOver()
+    {
+        gameOverScreen.SetActive(true);
+    }
+
 }
